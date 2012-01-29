a57ecb8ca8706b7edc6cbe4285a947600b88ccda
Alex Palcuie <alex.palcuie@gmail.com>
2012-01-29 21:09:30 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Wordpress theme for http://cozia.leoclubs.ro/, a local Romanian NGO, based on Twentyten.
+
+![screenshot](https://github.com/palcu/leocozia-wp/raw/master/screenshots/s1_leo.png)
+
+[License](http://wordpress.org/about/gpl/)
---
+++ wp-content/themes/twentyten-leo/frontpage.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Template Name: FrontPage
+*/
+?>
+<?php get_header(); ?>
+		<div id="container">
+			<div id="content" role="main">
+
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+					<div class="entry-content">
+						<?php the_content(); ?>
+						<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?>
+					</div><!-- .entry-content -->
+				</div><!-- #post-## -->
+
+<?php endwhile; ?>
+
+			</div><!-- #content -->
+		</div><!-- #container -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
